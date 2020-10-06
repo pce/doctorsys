28ee13f6de6209288b8e36d001fdd028c947ba71
pce <445226+pce@users.noreply.github.com>
2020-10-06 23:22:43 +0200

    celanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,7 @@
 #include "system.h"
 #include "system_linux.h"
 
-
 int main() {
-  // System factory instead of: 
-  // std::shared_ptr<SystemLinux> sys = std::make_unique<SystemLinux>();
   std::shared_ptr<System> sys = System::Factory();
   Screen::Display(sys);
 }
